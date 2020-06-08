59efe24925bbdfd113a8dd120cd11fdc51888a36
Merlog <jakub.fides@gmail.com>
2020-06-08 08:48:56 +0200

    creating item and deleting item

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,34 @@
 <template>
   <div id="app">
     <h1>{{ msg }}</h1>
+    <app-new-item @itemAdded="newItem"></app-new-item>
+    <app-item-grid :items="items" @itemDeleted="deleteItem"></app-item-grid>
   </div>
 </template>
 
 <script>
+import ItemGrid from "./components/ItemGrid.vue";
+import NewItem from "./components/NewItem.vue";
+
 export default {
   name: "app",
   data() {
     return {
-      msg: "todo list"
+      msg: "todo list",
+      items: ["Sell a car"]
     };
+  },
+  methods: {
+    newItem(item) {
+      this.items.push(item);
+    },
+    deleteItem(index) {
+      this.items.splice(index, 1);
+    }
+  },
+  components: {
+    appItemGrid: ItemGrid,
+    appNewItem: NewItem
   }
 };
 </script>
@@ -22,12 +40,12 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin-top: 30px;
+  padding: 40px;
 }
 
 h1,
 h2 {
   font-weight: normal;
 }
-
 </style>
---
+++ src/components/ItemGrid.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="row">
+    <app-item v-for="(item, index) in items" @click.native="deleteItem(index)">{{ item }}</app-item>
+  </div>
+</template>
+
+<script>
+import Item from "./Item.vue";
+
+export default {
+  props: ["items"],
+  components: {
+    appItem: Item
+  },
+  methods: {
+    deleteItem(index) {
+      this.$emit("itemDeleted", index);
+    }
+  }
+};
+</script>
+
+<style>
+</style>
